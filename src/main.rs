dd04ec3af82ef35c9735d23c95d34feb29c86497
vaporyhumo <roanvilina@gmail.com>
2024-05-07 22:08:42 -0400

    Update `main.rs`

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,33 @@
-fn main() {
-  println!("Hello, world!");
+mod token;
+mod vm;
+mod node;
+
+use token::Token;
+
+fn main() { }
+
+
+pub struct BalancedTokens {
+  pub tokens: Vec<Token>,
 }
+
+impl BalancedTokens {
+  pub fn new(tokens: Vec<Token>) -> Self {
+    let mut brackets: usize = 0;
+
+    for token in tokens.iter() {
+      match token {
+        Token::Open => brackets += 1,
+        Token::Close => brackets -= 1,
+        _ => (),
+      }
+    }
+
+    if brackets != 0 {
+      panic!("Unbalanced brackets");
+    }
+
+    Self { tokens }
+  }
+}
+
