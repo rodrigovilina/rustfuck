48a48bb4062c1691d8e95655fbb462867a83f3a3
Rodrigo Vilina <roanvilina@gmail.com>
2025-04-11 23:13:27 -0400

    Remove unused code

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,7 @@
 use {
   crate::{lexer::lex, parser::parse, vm::Brainfuck},
   node::Node,
-  std::{
-    io::{self, BufRead, Error, ErrorKind, Read},
-    str::from_utf8,
-  },
+  std::io::{self, BufRead},
   token::BalancedTokens,
 };
 
@@ -55,25 +52,4 @@ fn run_file(args: Vec<String>) {
 
   vm.run(nodes);
   vm.debug();
-}
-
-pub fn read_char_from_stdin() -> char {
-  let mut buffer = [0; 4];
-  let mut stdin = io::stdin();
-
-  stdin.read_exact(&mut buffer[0..1]).unwrap();
-  let mut len = 1;
-  if from_utf8(&buffer[0..len]).is_err() {
-    while len < 4 && (buffer[len - 1] & 0b11000000) == 0b10000000 {
-      stdin.read_exact(&mut buffer[len..len + 1]).unwrap();
-      len += 1;
-    }
-  }
-
-  from_utf8(&buffer[0..len])
-    .unwrap()
-    .chars()
-    .next()
-    .ok_or(Error::new(ErrorKind::UnexpectedEof, "No character found"))
-    .unwrap()
 }
